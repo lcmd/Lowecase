17a874acafe2fba9cd4a5d7b9d719f165eff1980
Your Name <your@example.com>
2016-05-26 10:52:19 -0400

    initial setup

---
+++ lowercase code_eval/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lowercase_code_eval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "This is some text";
+            Console.WriteLine(line.ToLower());
+        }
+    }
+}
